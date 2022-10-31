60f1888154b6d3a443262f98d7d87b4abfab55a9
AlphaS <ar.su0104@gmail.com>
2022-10-31 16:16:39 +0000

    Update ESLint

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-const threads = require("node:worker_threads")
-const ClientProcess = new threads.Worker("./handlers/client.js")
-const ServerProcess = new threads.Worker("./handlers/server.js")
+const threads = require('node:worker_threads')
+const ClientProcess = new threads.Worker('./handlers/client.js')
+const ServerProcess = new threads.Worker('./handlers/server.js')
